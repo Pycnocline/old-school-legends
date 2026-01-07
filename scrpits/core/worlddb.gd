4f4fe7dd0e987339231a03a90f001879d29bfae3
密度跃层 <pycnocline@hotmail.com>
2026-01-07 20:50:46 +0800

    更新了一点目的地系统

---
+++ scrpits/core/worlddb.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var destinations:Dictionary
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
--- scrpits/resource/destination.gd
+++ scrpits/resource/destination.gd
@@ -1,1 +1,10 @@
+class_name Destination
 extends Resource
+
+@export_group("目的地信息")
+@export var id: String
+@export var name: String
+@export_multiline var description: String
+
+@export_group("连接关系")
+@export var exits: Array[ExitData]
