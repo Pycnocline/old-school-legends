c382349cbdf4eeb5d7e27cee78a4b0bc6330f0af
密度跃层 <pycnocline@hotmail.com>
2026-01-11 22:04:44 +0800

    更新行动

--- scrpits/core/game_host.gd
+++ scrpits/core/game_host.gd
@@ -1,14 +1,36 @@
 extends Node
 
+var last_control_time_stamp: int = Time.get_unix_time_from_system()
+
+var available_action: Array = [
+		"[url=" + str(last_control_time_stamp) + "/0][wave]>> 移动 <<[/wave][/url]",
+		"[url=" + str(last_control_time_stamp) + "/1][wave]>> 等待 <<[/wave][/url]",
+		"[url=" + str(last_control_time_stamp) + "/2][wave]>> 暂停 <<[/wave][/url]",
+		"[url=" + str(last_control_time_stamp) + "/3][wave]>> 背包 <<[/wave][/url]",
+		"[url=" + str(last_control_time_stamp) + "/4][wave]>> 观察四周 <<[/wave][/url]",
+	]
+
 func show_info_panel() -> void:
-	SignalBus.message_output.emit(" ")
+	last_control_time_stamp = Time.get_unix_time_from_system()
+	
 	SignalBus.message_output.emit("[hr]")
-	SignalBus.message_output.emit(TimeSystem.get_time_string())
-	SignalBus.message_output.emit(Gamedb.player_state.name)
-	SignalBus.message_output.emit("当前位置：" + Gamedb.player_state.position.name)
-	var data_text: String = ""
+	SignalBus.message_output.emit(TimeSystem.get_time_string() + "  -  " + Gamedb.player_state.position.name)
+	SignalBus.message_output.emit("▼[队伍状态信息]--------------------")
+	var data_text: String = "    >> " + Gamedb.player_state.name + "    "
 	for data in Gamedb.player_state.data:
 		data_text += data.name + " " + str(data.value) + "/" + str(data.upper_limit) + " | "
 	SignalBus.message_output.emit(data_text)
-	SignalBus.message_output.emit("[hr]")
-	SignalBus.message_output.emit(" ")
+	SignalBus.message_output.emit("▼[可用行动列表]--------------------")
+	
+	var object = 0
+	var action_text:String = "    "
+	for action in available_action:
+		object += 1
+		action_text += action + "          "
+		if object % 4 == 0:
+			SignalBus.message_output.emit(action_text)
+			action_text = "    "
+	if action_text != "":
+		SignalBus.message_output.emit(action_text)
+	
+	
--- scrpits/interface/output_manager.gd
+++ scrpits/interface/output_manager.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var main_output_rich_text_label: RichTextLabel = $"../../MainOutput_RichTextLabel"
 
+var output_queue:Array[String] = []
+
 func _ready() -> void:
 	initialize_main_output()
 	handle_signal_connection()
@@ -18,4 +20,10 @@ func handle_signal_connection() -> void:
 	SignalBus.message_output.connect(_handle_message_output)
 
 func _handle_message_output(output_message) -> void:
-	main_output_rich_text_label.text = main_output_rich_text_label.text + "\n" + output_message
+	output_queue.append(output_message)
+
+
+func _on_output_timer_timeout() -> void:
+	var message_to_add = output_queue.pop_front()
+	if message_to_add != null:
+		main_output_rich_text_label.text = main_output_rich_text_label.text + "\n" + message_to_add
